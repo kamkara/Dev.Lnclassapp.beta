fdf0e07477ca27cc129be5d679e5ff1e91e58368
kamkara <lnclass.kamkara@gmail.com>
2022-05-23 11:56:35 +0000

    app/controllers/courses_controller: add redirect to root_path

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -57,7 +57,7 @@ class CoursesController < ApplicationController
     @course.destroy
 
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
+      format.html { redirect_to root_path, notice: "Course was successfully destroyed." }
       format.json { head :no_content }
     end
   end
